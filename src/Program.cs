5cc919b53c3cdd57a8f0681853e24e019d23155a
jstrode84 <u0348058@umail.utah.edu>
2018-10-18 14:34:20 -0600

    Revert "Add <li> to View along with some .vs info"
    This reverts commit 71c07d77ebab5282977c20bfe759b2894a65a5bc.

--- src/Program.cs
+++ src/Program.cs
@@ -9,7 +9,7 @@
     {
         static void Main()
         {
-            var hostname = "aqueous-ocean-35636";
+            var hostname = "deep-moon-1452";
 
             var port = System.Environment.GetEnvironmentVariable("PORT");
             var nancyHost = new NancyHost(new Uri("http://" + hostname + ".herokuapp.com:" + port), new Uri("http://0.0.0.0:" + port));
